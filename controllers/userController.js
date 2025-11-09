b39df68a68cd29fbcb7f973d43acc492145aac49
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-11-09 10:51:12 +0530

    implement user authentication with create and login functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,18 @@
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 export function createUser(req,res){
 
+ const newUserData = req.body
+
+ newUserData.password = bcrypt.hashSync(newUserData.password, 10)
+
+ 
     
-    const user = new User(req.body)
+    const user = new User(newUserData)
+
     user.save().then(()=>{
         res.json({
             message : "User created successfully"
@@ -13,6 +22,36 @@ export function createUser(req,res){
             res.json({
                 message : "Error creating user"
             })
+        }
+    )
+}
+
+export function loginUser(req,res){
+
+    User.find({email : req.body.email}).then(
+        (users)=>{
+            if(users.length == 0){
+
+                res.json({
+                    message : "User not found"
+                })
+            }else{
+
+                const user = users[0]
+
+                const isPasswordValid = bcrypt.compareSync(req.body.password, user.password)
+
+                if(isPasswordValid){
+                    res.json({
+                        message : "Login successful"
+                    })
+                }else{
+                    res.json({
+                        message : "Invalid password"
+                    })
+                }
+
+            } 
         }
     )
 }
