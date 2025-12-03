fbf4c362d07d7f48646370ad727ef5b0e0fda528
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-03 23:11:58 +0530

    feat: implement createOrder function for processing customer orders

---
+++ controllers/orderController.js
@@ -0,0 +1,40 @@
+import Order from '../models/order.js';
+import { isCustomer } from './userController.js';
+
+export async function createOrder(req, res) {
+
+    if (!isCustomer(req)) {
+        return res.json({
+            message: "Only customers can place orders"
+        });
+    }
+
+    try {
+        const latestOrder = await Order.find().sort({ date: -1 }).limit(1);
+
+        let orderId;
+        if (latestOrder.length === 0) {
+            orderId = "cbc001";
+        } else {
+            const currentOrderId = latestOrder[0].orderId;
+            const number = parseInt(currentOrderId.replace("cbc", ""));
+            orderId = "cbc" + (number + 1).toString().padStart(4, '0');
+        }
+
+        const newOrderData = req.body;
+        newOrderData.orderId = orderId;
+        newOrderData.email = req.user.email;
+
+        const order = new Order(newOrderData);
+        await order.save();
+
+        res.json({
+            message: "Order placed successfully",
+            orderId
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
