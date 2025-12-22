814a4ca396e7a22644a57f41b5b3aaa6aa69d1cf
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-23 02:15:57 +0530

    feat: implement LoginPage component with basic structure

---
+++ cbc-frontend/src/pages/loginPage.jsx
@@ -0,0 +1,13 @@
+export default function LoginPage() {
+    return (
+        <div>
+            <h1>Login Page </h1>
+            <input type="text" placeholder="Enter your Username" />
+            <input type="password" placeholder="Enter your Password" />
+            <button>Login</button>  
+        </div>
+        
+
+        
+    )
+}
