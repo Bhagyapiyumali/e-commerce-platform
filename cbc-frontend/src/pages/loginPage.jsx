ed0a4f4be6668dc359d83e3294a2387c628a0478
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-24 02:25:16 +0530

    feat: enhance LoginPage layout with CSS classes for better styling

--- cbc-frontend/src/pages/loginPage.jsx
+++ cbc-frontend/src/pages/loginPage.jsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
+import './loginPage.css';
+
 export default function LoginPage() {
     return (
-        <div>
-            <h1>Login Page </h1>
+        <div className="login-container">
+            <h1 className="login-title">Login Page </h1>
             <input type="text" placeholder="Enter your Username" />
             <input type="password" placeholder="Enter your Password" />
             <button>Login</button>  
+
+            <Link to="/">Back to Home Page</Link>   
         </div>
         
 
