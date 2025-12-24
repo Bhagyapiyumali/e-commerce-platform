c4f4b3d40e2884aef410db0b5bdf5ddd8c5163a8
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-25 03:43:57 +0530

    add new pages

--- cbc-frontend/src/App.jsx
+++ cbc-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import LoginPage from './pages/loginPage'
 import HomePage from './pages/homePage'
+import SignUpPage from './pages/signUpPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
@@ -16,7 +17,8 @@ function App() {
         <Routes path="/*">
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
+          <Route path="/signup" element={<SignUpPage/>}/>
+          <Route path="*" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </>
