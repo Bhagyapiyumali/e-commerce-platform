5afbb310b5373b1c8ac8a969f916fbd7b2bd29ac
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2026-01-02 01:27:25 +0530

    FEat: add correct pth with admin panel

--- cbc-frontend/src/App.jsx
+++ cbc-frontend/src/App.jsx
@@ -8,6 +8,10 @@ import SignUpPage from './pages/signUpPage'
 import AdminHomePage from './pages/adminHomePage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast'
+import AdminProductPage from './pages/admin/adminProductPage'
+import AdminCustomerPage from './pages/admin/adminCustomerPage'
+import AdminDashboardPage from './pages/admin/adminDashboardPage'
+import AdminOrderPage from './pages/admin/adminOrderPage'
 
 
 function App() {
@@ -21,7 +25,14 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage/>}/>
-          <Route path="/admin/*" element={<AdminHomePage/>}/>
+          <Route path="/admin" element={<AdminHomePage />}>
+          
+          <Route path="products" element={<AdminProductPage />} />
+          <Route path="customers" element={<AdminCustomerPage />} />
+          <Route path="dashboard" element={<AdminDashboardPage />} />
+          <Route path="orders" element={<AdminOrderPage />} />
+
+        </Route>
           <Route path="*" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
