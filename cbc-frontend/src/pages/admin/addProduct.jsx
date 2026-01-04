045ed90161441d44b44b6815305400ed0f3a4d1c
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2026-01-04 19:48:00 +0530

    feat: add new product page

---
+++ cbc-frontend/src/pages/admin/addProduct.jsx
@@ -0,0 +1,12 @@
+export default function AddProductPage() {
+    return (
+        <div className="w-full h-full bg-blue-200">
+            <h1 className="text-2xl text-center">
+                Add Product Form
+
+            </h1>
+            <input type="text" placeholder="Insert your product name here"/>
+            
+        </div>
+    )
+}
