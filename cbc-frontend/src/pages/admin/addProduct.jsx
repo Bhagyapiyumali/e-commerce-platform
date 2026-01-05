b2a4c5e9e0ab17df38831d9288611cabf5ca0cfd
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2026-01-05 23:14:34 +0530

    feat: enhance Add Product page layout with styled input fields

--- cbc-frontend/src/pages/admin/addProduct.jsx
+++ cbc-frontend/src/pages/admin/addProduct.jsx
@@ -5,8 +5,13 @@ export default function AddProductPage() {
                 Add Product Form
 
             </h1>
-            <input type="text" placeholder="Insert your product name here"/>
-            
+            <div className="bg-white p-4 rounded shadow-md mt-4">
+                <input type="text" placeholder="Insert your product name here" className="border border-gray-300 rounded px-3 py-2 w-full mb-2"/>
+                <input type="text" placeholder="Insert your product price here" className="border border-gray-300 rounded px-3 py-2 w-full mb-2"/>
+                <input type="text" placeholder="Insert your product description here" className="border border-gray-300 rounded px-3 py-2 w-full mb-2"/>
+                <input type="text" placeholder="Insert your product image url here" className="border border-gray-300 rounded px-3 py-2 w-full mb-2"/>
+                <button className="bg-blue-500 text-black px-4 py-2 rounded mt-4">Add Product</button>
+            </div>
         </div>
     )
 }
