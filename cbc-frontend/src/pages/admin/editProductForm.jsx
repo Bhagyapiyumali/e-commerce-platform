02dddad536834f65a5d6fd1a374d2b830046cd76
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2026-03-12 05:06:45 +0530

    fix: update import path for media upload utility in EditProductForm

--- cbc-frontend/src/pages/admin/editProductForm.jsx
+++ cbc-frontend/src/pages/admin/editProductForm.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
-import uploadMediaToSupabase from '../utils/mediaUpload';
+import uploadMediaToSupabase from '../../utils/mediaUpload';
 import { useLocation } from 'react-router-dom';
 
 
