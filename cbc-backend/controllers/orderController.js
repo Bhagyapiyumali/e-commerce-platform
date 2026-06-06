868ec45d9eb80d4ded9f1ff1f8589c12de3e5a0c
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2026-06-07 04:59:29 +0530

    feat: enhance order management with admin access and quote generation

--- cbc-backend/controllers/orderController.js
+++ cbc-backend/controllers/orderController.js
@@ -1,6 +1,6 @@
 import Order from '../models/order.js';
 import Product from '../models/product.js';
-import { isCustomer } from './userController.js';
+import { isCustomer, isAdmin } from './userController.js';
 
 export async function createOrder(req, res) {
 
@@ -38,7 +38,7 @@ export async function createOrder(req, res) {
                 return res.json({
                     message: "Product with ID "+newOrderData.orderedItems[i].productId+" not found"
                 });
-                return;
+                
             }
 
             newProductArray[i] = {
@@ -56,11 +56,11 @@ export async function createOrder(req, res) {
         newOrderData.email = req.user.email;
 
         const order = new Order(newOrderData);
-        await order.save();
+        const savedOrder = await order.save();
 
         res.json({
             message: "Order created successfully",
-            
+            order : savedOrder
         });
 
     } catch (error) {
@@ -70,11 +70,120 @@ export async function createOrder(req, res) {
 
 export async function getOrders(req, res) {
     try {
+        if (isCustomer(req)) {
         const orders = await Order.find({ email: req.user.email });
         res.json(orders);
+        return;
+        } else if(isAdmin(req)) {
+            const orders = await Order.find({});
+            res.json(orders);
+            return;
+        }else {
+            res.json({
+                message: "Please login to view orders"
+            });
+            
+        }
+
 
         
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
+}
+
+export async function getQuote(req, res) {
+  
+  try {
+    const newOrderData = req.body;
+
+    const newProductArray = [];
+
+    let total = 0;
+    let labeledTotal = 0;
+    console.log(req.body)
+
+    for (let i = 0; i < newOrderData.orderedItems.length; i++) {
+      const product = await Product.findOne({
+        productId: newOrderData.orderedItems[i].productId,
+      });
+
+      if (product == null) {
+        res.json({
+          message:
+            "Product with id " +
+            newOrderData.orderedItems[i].productId +
+            " not found",
+        });
+        return;
+      }
+      labeledTotal += product.price * newOrderData.orderedItems[i].qty;
+      total += product.lastPrice * newOrderData.orderedItems[i].qty;
+      newProductArray[i] = {
+        name: product.productName,
+        price: product.lastPrice,
+        labeledPrice: product.price,
+        quantity: newOrderData.orderedItems[i].qty,
+        image: product.images[0],
+      };
+    }
+    console.log(newProductArray);
+    newOrderData.orderedItems = newProductArray;
+    newOrderData.total = total;
+
+    res.json({
+      orderedItems: newProductArray,
+      total: total,
+      labeledTotal: labeledTotal,
+    });
+
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+}
+
+export async function updateOrder(req, res) {
+  if (!isAdmin(req)) {
+    res.json({
+      message: "Please login as admin to update orders",
+    });
+  }
+  
+  try {
+    const orderId = req.params.orderId;
+
+    const order = await Order.findOne({
+      orderId: orderId,
+    });
+
+    if (order == null) {
+      res.status(404).json({
+        message: "Order not found",
+      })
+      return;
+    }
+
+    const notes = req.body.notes;
+    const status = req.body.status;
+
+    const updateOrder = await Order.findOneAndUpdate(
+      { orderId: orderId },
+      { notes: notes, status: status }
+    );
+
+    res.json({
+      message: "Order updated",
+      updateOrder: updateOrder
+    });
+
+  }catch(error){
+
+    
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 }
