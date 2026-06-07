18b145fdd769e99469102748b85e2b89ceb05dfb
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2026-06-07 23:03:03 +0530

    style: update product display layout to a grid for better organization

--- cbc-frontend/src/pages/home/product.jsx
+++ cbc-frontend/src/pages/home/product.jsx
@@ -69,7 +69,7 @@ export default function ProductPage() {
 
       {/* ================= PRODUCTS ================= */}
       {loadingStatus == "loaded" && ( 
-        <div className="w-full h-full overflow-y-scroll flex flex-wrap justify-center pt-4 relative"> 
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 px-8 py-8 place-items-center"> 
         {products.map((product) => ( 
           <ProductCard product={product} />
         ))} 
